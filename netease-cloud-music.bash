141e5ccac8c021cff1921a25b8bf9e25d6b1f948
幸福的路痴 <maoxuner@126.com>
2021-02-22 11:23:21 +0800

    Fix xfce/kde tray icon

--- netease-cloud-music.bash
+++ netease-cloud-music.bash
@@ -1,6 +1,7 @@
 #!/bin/sh
 HERE="$(dirname "$(readlink -f "${0}")")"
-export LD_LIBRARY_PATH=/usr/lib  # 注意这里的变化
+export LD_LIBRARY_PATH=/usr/lib	# 注意这里的变化
 export QT_PLUGIN_PATH="${HERE}"/plugins
 export QT_QPA_PLATFORM_PLUGIN_PATH="${HERE}"/plugins/platforms
+export XDG_CURRENT_DESKTOP=DDE	# 修复XFCE、KDE等状态栏图标的问题(折衷方案)
 exec "${HERE}"/netease-cloud-music $@
